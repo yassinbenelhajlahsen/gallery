6a26bbb0b44780f9c42ff90e5389b1b2ee7c2b29
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-02-19 16:50:42 -0500

    remove legacy firebaseConfig file

--- src/services/firebaseConfig.ts
+++
@@ -1,7 +0,0 @@
-// src/services/firebaseConfig.ts
-// Backward-compatible barrel for legacy imports.
-// Prefer importing from firebaseApp/firebaseAuth/firebaseFirestore/firebaseStorage directly.
-export { app, firebaseConfig } from "./firebaseApp";
-export { auth } from "./firebaseAuth";
-export { db } from "./firebaseFirestore";
-export { storage } from "./firebaseStorage";
