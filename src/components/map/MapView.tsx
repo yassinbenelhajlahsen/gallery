2383662b6c31bf08ab383bbe3b4f08ab9b7a14de
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-04-18 09:55:03 -0400

    feat: disable removal of clusters outside visible bounds in ClusterLayer

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -76,6 +76,7 @@ const ClusterLayer: React.FC<Props> = ({ items, onClusterSelect }) => {
       showCoverageOnHover: false,
       zoomToBoundsOnClick: false,
       spiderfyOnMaxZoom: false,
+      removeOutsideVisibleBounds: false,
       maxClusterRadius: 30,
       iconCreateFunction: (c) => buildClusterIcon(c.getChildCount()),
     });
